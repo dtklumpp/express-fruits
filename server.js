3ba74b535886e8eafae3b2a319294c566a57f86e
micahwierenga <wierenga.micah@gmail.com>
2020-09-09 11:17:32 -0600

    Adds new and create routes

--- server.js
+++ server.js
@@ -4,6 +4,35 @@ const PORT = 3000;
 // return whatever is exported from the file
 const fruits = require('./models/fruit_model.js');
 
+
+// middleware
+// takes POST and PUT requests and adds form data to request object
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+})
+
+
+// new route
+app.get('/fruits/newForm', (req, res) => {
+    res.render('new.ejs');
+})
+
+// create route
+app.post('/fruits', (req, res) => {
+    // console.log('REQ.BODY:', req.body);
+    if(req.body.readyToEat === 'on') {
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    // console.log('FRUITS:', fruits);
+    // redirect is a GET request
+    res.redirect('/fruits');
+})
+
 // show route
 app.get('/fruits/:fruitIndex', (req, res) => {
     // By default, Express will look inside the views
@@ -16,7 +45,9 @@ app.get('/fruits/:fruitIndex', (req, res) => {
 
 // index route
 app.get('/fruits', (req, res) => {
-    res.send(fruits);
+    res.render('index.ejs', {
+        allFruits: fruits
+    })
 })
 
 
