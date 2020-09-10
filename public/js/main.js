3994ab5338296222d23e103d204417b1d296e1b5
micahwierenga <wierenga.micah@gmail.com>
2020-09-10 10:39:00 -0600

    Adds public assets

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+// console.logs in this file will output in the browser console
+// this is where you'd do DOM manipulation
+console.log('fruits front-end');
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 // return whatever is exported from the file
 const fruits = require('./models/fruit_model.js');
 
-
 // middleware
 // takes POST and PUT requests and adds form data to request object
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.originalUrl}`);
